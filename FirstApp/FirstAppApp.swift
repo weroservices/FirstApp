f0f1e4d3c5c802352888a42096192ea7ac80d497
weroservices <werner.rodenbusch@gmx.net>
2021-08-22 11:16:51 +0200

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Werner on 22.08.21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
